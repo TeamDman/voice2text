669c585586df687a27e72104fabd6fe4b28905b0
TeamDman <TeamDman9201@gmail.com>
2024-09-14 12:30:08 -0400

    activate conda properly

--- start.ps1
+++ start.ps1
@@ -1,3 +1,10 @@
 Write-Host -ForegroundColor "Green" Loading voice2text
+
+## in pwsh profile
+# function bonda() {
+#     (& "C:\ProgramData\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+# }
+
+bonda
 conda activate whisperx
 python ./transcribe_hotkey_typer.py
