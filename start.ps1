d4b55b5172fea3c11df411c77c856ed55719bd94
TeamDman <TeamDman9201@gmail.com>
2026-01-12 17:45:27 -0500

    switch to uv

--- start.ps1
+++ start.ps1
@@ -5,6 +5,9 @@ Write-Host -ForegroundColor "Green" Loading voice2text
 #     (& "C:\ProgramData\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
 # }
 
-bonda
-conda activate whisperx
-python ./transcribe_hotkey_typer.py
+# bonda
+# conda activate whisperx
+# python ./transcribe_hotkey_typer.py
+
+$Env:TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD="true"
+uv run .\transcribe_hotkey_typer.py
